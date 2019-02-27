367ada34bdddea1eced6bb5f1e46ba882417baad
prthomas <preethu.thomas@cotiviti.com>
2019-02-26 19:19:06 -0500

    Add tests for default values

--- test_splitunzipper.py
+++ test_splitunzipper.py
@@ -3,8 +3,10 @@ import unittest
 from splitunzipper import SplitUnzipper
 
 class TestSplitUnzipper(unittest.TestCase):
-  def test_defineobject(self):
+  def test_defineobject_checkdefaults(self):
     testsplitunzipper = SplitUnzipper(ZipFile="")
+    assert(testsplitunzipper.splitsize == 128 * 1024 * 1024)
+    assert(testsplitunzipper.headerincl == False)
 
 if __name__ == '__main__':
     unittest.main()
